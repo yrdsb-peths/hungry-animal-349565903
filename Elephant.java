0246c3da1c58be5825a11a7738a96e75195c5d63
349565903 <118018441+349565903@users.noreply.github.com>
2022-12-12 14:16:00 -0500

    Name: Hoffman
    Name: Hoffman

--- Elephant.java
+++ Elephant.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * The Elephant, our hero 
  * 
- * @author (your name) 
+ * @author  Hoffman
  * @version (a version number or a date)
  */
 public class Elephant extends Actor
