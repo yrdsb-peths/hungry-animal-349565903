570fd269f20182bb81949bfe377a4fb2bd75b432
349565903 <118018441+349565903@users.noreply.github.com>
2022-12-12 14:14:13 -0500

    Elephant our hero
    Elephant our hero

--- Elephant.java
+++ Elephant.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Elephant here.
+ * The Elephant, our hero 
  * 
  * @author (your name) 
  * @version (a version number or a date)
